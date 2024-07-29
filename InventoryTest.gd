df482b03f2226e24c5b570beada82b2135fad5f4
btarg <icrazyblaze1@gmail.com>
2024-07-29 01:29:07 +0100

    i like gdscript

--- InventoryTest.gd
+++ InventoryTest.gd
@@ -5,10 +5,9 @@ var inventory: Node
 var test_item := preload("res://Scripts/Inventory/Resources/new_item.tres") as BaseInventoryItem
 var test_item2 := preload("res://Scripts/Inventory/Resources/new_item2.tres") as BaseInventoryItem
 
-
 func _ready():
 	# Create some items
-	InventoryManager.connect("inventory_updated", update_inventory)
+	InventoryManager.connect("inventory_updated", Callable(self, "update_inventory"))
 	
 	# Add items to the inventory
 	InventoryManager.add_item(test_item2, 2)
@@ -17,22 +16,39 @@ func _ready():
 	InventoryManager.print_inventory()
 	
 func update_inventory(item: BaseInventoryItem, count: int, is_new_item: bool) -> void:
-
 	if is_new_item:
 		# Create a new item display
 		var item_label = preload("res://item_display.tscn").instantiate()
 		# Set the text of the item display
-		item_label.text = item.get_rich_name() + " x" + str(count)
+		item_label.text = item.item_name + " x" + str(count)
+		item_label.icon = load(item.get_icon_path())
+		# Connect the button press signal and bind the item as a parameter
+		item_label.connect("pressed", _on_item_label_pressed.bind(item))
+
 		# Set the name of the item display to the item ID
 		item_label.name = item.item_id
 		# Add the item display to the inventory list
 		$InventoryList.add_child(item_label)
+		# if it is the first item, focus it
+		if $InventoryList.get_child_count() == 1:
+			item_label.grab_focus()
+
 	else:
 		# Find the item display by name
 		var item_label = $InventoryList.get_node(item.item_id)
 		# Update the text of the item display
-		item_label.text = item.get_rich_name() + " x" + str(InventoryManager.get_item_count(item.item_id))
+		item_label.text = item.item_name + " x" + str(InventoryManager.get_item_count(item.item_id))
+		#item_label.text = item.get_rich_name() + " x" + str(InventoryManager.get_item_count(item.item_id))
 
+func _on_item_label_pressed(item: BaseInventoryItem) -> void:
+	print("Button clicked for item: " + item.item_name + " x" + str(InventoryManager.get_item_count(item.item_id)))
+	var status = item.use()
+	if status == BaseInventoryItem.UseStatus.CONSUMED_HP or status == BaseInventoryItem.UseStatus.CONSUMED_MP:
+		print("Item consumed")
+	elif status == BaseInventoryItem.UseStatus.CANNOT_USE:
+		print("Item cannot be used")
+	elif status == BaseInventoryItem.UseStatus.EQUIPPED:
+		print("Item equipped")
 
 # on input add an item
 func _input(event):
--- Scripts/Inventory/Item.gd
+++ Scripts/Inventory/Item.gd
@@ -9,6 +9,12 @@ enum ItemType {
 	QUEST,
 	MISC
 }
+enum UseStatus {
+	CONSUMED_HP,
+	CONSUMED_MP,
+	CANNOT_USE,
+	EQUIPPED
+}
 
 # Declare the class and its properties
 @export var item_type: ItemType = ItemType.WEAPON
@@ -22,8 +28,7 @@ func _init(_item_name: String = "", _max_stack: int = 999):
 	self.item_name = _item_name
 	self.max_stack = _max_stack
 
-func get_rich_name(icon_size: int = 64) -> String:
-	# icon path switch statement based on enum type
+func get_icon_path() -> String:
 	var icon_path: String = ""
 	match item_type:
 		ItemType.WEAPON:
@@ -41,5 +46,20 @@ func get_rich_name(icon_size: int = 64) -> String:
 			icon_path = "res://Assets/Icons/item_misc.png"
 		_:
 			icon_path = "res://Assets/Icons/item_misc.png"
+	return icon_path
 
+func get_rich_name(icon_size: int = 64) -> String:
+	var icon_path = get_icon_path()
 	return "[hint=%s][img=%s]%s[/img]%s[/hint]" % [item_description, icon_size, icon_path, item_name]
+
+
+func use() -> UseStatus:
+	if item_type == ItemType.CONSUMABLE_HP:
+		print("HP Item used: %s" % item_name)
+		return UseStatus.CONSUMED_HP
+	elif item_type == ItemType.CONSUMABLE_MP:
+		print("MP Item used: %s" % item_name)
+		return UseStatus.CONSUMED_MP
+	else:
+		print("Item cannot be consumed: %s" % item_name)
+		return UseStatus.CANNOT_USE
